8d67ac22fe002b15b06434c6423a5c58ba4c33e3
Raito <hieunx@medinetgroup.vn>
2023-08-23 17:44:02 +0700

    23/08/2023

--- app/js/main.js
+++ app/js/main.js
@@ -61,15 +61,15 @@ var slider={
   list_home_brand: function() {
     var list_slide=$(".home-brand .list-home-brand");
     var number_slide=$(".home-brand .list-home-brand .item-brand").length;
-    if(list_slide.length>0&&number_slide>=3) {
+    if(list_slide.length>0&&number_slide>6) {
       list_slide.slick({
         dots: false,
         arrow: true,
         speed: 1000,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 6,
-        slidesToScroll: 1,
+        slidesToScroll: 6,
         responsive: [
           {
             breakpoint: 575,
@@ -87,9 +87,9 @@ var slider={
     }
   },
   js_list_post: function() {
-    var list_slide=$(".list-post-bottom .js-list-post");
-    var number_slide=$(".list-post-bottom .js-list-post .post-grid").length;
-    if(list_slide.length>0&&number_slide>=3) {
+    var list_slide = $(".list-post-bottom .js-list-post");
+    var number_slide = $(".list-post-bottom .js-list-post .post-grid").length;
+    if(list_slide.length > 0 && number_slide >3) {
       list_slide.slick({
         dots: false,
         arrow: false,
@@ -97,14 +97,13 @@ var slider={
         // autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         responsive: [
           {
             breakpoint: 575,
             settings: {
               slidesToShow: 1.5,
               slidesToScroll: 1,
-              infinite: true,
               dots: false,
               centerMode: false,
               arrows: false,
@@ -167,7 +166,7 @@ var slider={
     var number_slide=$(".home-team-experts .home-list-author .item-author").length;
     if(list_slide.length>0&&number_slide>=3) {
       list_slide.slick({
-        dots: true,
+        dots: false,
         arrow: false,
         speed: 1000,
         autoplaySpeed: 3000,
@@ -303,17 +302,17 @@ var slider={
         speed: 1000,
         autoplaySpeed: 3000,
         slidesToShow: 2,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         responsive: [
           {
             breakpoint: 575,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 1,
               slidesToScroll: 1,
               infinite: true,
-              dots: true,
+              dots: false,
               centerMode: false,
-              arrows: false,
+              arrows: true,
             },
           },
         ],
@@ -330,17 +329,17 @@ var slider={
         speed: 1000,
         autoplaySpeed: 3000,
         slidesToShow: 2,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         responsive: [
           {
             breakpoint: 575,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 1,
               slidesToScroll: 1,
               infinite: true,
-              dots: true,
+              dots: false,
               centerMode: false,
-              arrows: false,
+              arrows: true,
             },
           },
         ],
@@ -420,6 +419,44 @@ var slider={
       });
     }
   },
+  list_affecting: function() {
+    var list_slide=$(".list-affecting");
+    var number_slide = $(".list-affecting .affecting-item").length;
+    if(list_slide.length>0&&number_slide>1) {
+      list_slide.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true,
+        arrows: false,
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 575,
+            settings: "unslick",
+          },
+        ],
+      });
+    }
+  },
+  list_affecting: function() {
+    var list_slide=$(".list-affecting-service");
+    var number_slide=$(".list-affecting-service .archive-item").length;
+    if(list_slide.length>0&&number_slide>1) {
+      list_slide.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true,
+        arrows: false,
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 575,
+            settings: "unslick",
+          },
+        ],
+      });
+    }
+  },
 };
 var show_menu_mb={
   init: function() {
@@ -427,14 +464,6 @@ var show_menu_mb={
     icon_menu.on("click",function() {
       $(".collapse-primary").toggleClass("active");
       return false;
-    });
-  },
-};
-var show_list_service_mb={
-  init: function() {
-    var list_service_cat=$(".experts-specialist");
-    list_service_cat.on("click",function() {
-      $(this).toggleClass("active");
     });
   },
 };
@@ -916,7 +945,6 @@ jQuery(document).ready(function() {
   search_banner.init();
   slider.init();
   show_menu_mb.init();
-  show_list_service_mb.init();
   show_menu.init();
   faq.init();
   smoothScroll.init();
